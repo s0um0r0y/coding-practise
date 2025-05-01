ad51f387196de9ff281f3223ce8993f5bf8b43bc
s0um0r0y <soumoroy09@gmail.com>
2025-05-02 01:24:44 +0530

    SLL + DLL linked list update

--- dsa_algo_practise/linked_list_practise.py
+++ dsa_algo_practise/linked_list_practise.py
@@ -6,20 +6,76 @@ class linkedList():
         def __init__(self, data):
             self.data = data
             self.next = None
+            self.prev = None
 
-    node1 = Node(3)
-    node2 = Node(5)
-    node3 = Node(12)
-    node4 = Node(2)
+    def singly_linked_list(self, Node):
+        self.node1 = Node(3)
+        self.node2 = Node(5)
+        self.node3 = Node(12)
+        self.node4 = Node(2)
 
-    node1.next = node2
-    node2.next = node3
-    node3.next = node4
+        self.node1.next = self.node2
+        self.node2.next = self.node3
+        self.node3.next = self.node4
 
-    currentNode = node1
-    while currentNode:
-        print(currentNode.data, end="->")
-        currentNode = currentNode.next
-    print("null")
-
+        currentNode = self.node1
+        while currentNode:
+            print(currentNode.data, end="->")
+            currentNode = currentNode.next
+        print("null")
+        return
     
+    def doubly_linked_list(self, Node):
+        self.node1.next = self.node2
+
+        self.node2.prev = self.node1
+        self.node2.next = self.node3
+
+        self.node3.prev = self.node2
+        self.node3.next = self.node3
+
+        self.node4.prev = self.node3
+
+    def traversing_forward(self):
+        currentNode = self.node1
+        while currentNode:
+            print(currentNode.data, end="-> ")
+            currentNode = currentNode.next
+        print("null")
+
+    def traversing_backward(self):
+        currentNode = self.node4
+        while currentNode:
+            print(currentNode.data, end="-> ")
+            currentNode = currentNode.next
+        print("null")
+
+    def circular_singly_linked_list(self):
+        self.node1.next = self.node2
+        self.node2.next = self.node3
+        self.node3.next = self.node4
+        self.node4.next = self.node1
+
+    # traversing_forward(circular_singly_linked_list)
+        # making list circular
+        currentNode = self.node1
+        startNode = self.node1
+        print(currentNode.data, end=" -> ") 
+        currentNode = currentNode.next 
+
+        while currentNode != startNode:
+            print(currentNode.data, end="-> ")
+            currentNode = currentNode.next
+
+    def circular_doubly_linked_list(self):
+        self.node1.next = next.node2
+        self.node1.prev = next.node4
+
+        self.node2.prev = next.node1
+        self.node2.next = next.node3
+
+        self.node3.prev = next.node2
+        self.node3.next = next.node4
+
+        self.node4.prev = next.node3
+        self.node4.next = next.node1
