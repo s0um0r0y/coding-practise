4c4e50b31968e5b16e473eceb64307aa90eb621a
s0um0r0y <soumoroy09@gmail.com>
2025-05-04 15:08:57 +0530

    stack with linked list

--- dsa_algo_practise/stack_practise.py
+++ dsa_algo_practise/stack_practise.py
@@ -21,8 +21,47 @@ class NormalStack:
     def size(self):
         return len(self.stack)
     
+# Stack implentation using a linked list
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedListStack:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.head:
+            new_node.next = self.head
+        self.head = new_node
+        self.size += 1
+
+    def pop(self):
+        if self.is_empty():
+            return "stack meh kuch nahi hai"
+        popped_node = self.head
+        self.head = self.head.next
+        self.size -= 1
+        return popped_node.value
+    
+    def peek(self):
+        if self.is_empty():
+            return "stack me kya hai bhai ?"
+        return self.head.value
+    
+    def is_empty(self):
+        return self.size == 0
+    
+    def stack_size(self):
+        return self.size
+
 # Create a stack
 myStack = NormalStack()
+myStack = LinkedListStack()
 
 myStack.push('A')
 myStack.push('B')
