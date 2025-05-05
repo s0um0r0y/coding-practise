fa1a6dd7d2bf8913207fae31352c89e6a10d4026
s0um0r0y <soumoroy09@gmail.com>
2025-05-06 03:33:52 +0530

    wrote forward loop, understood logits and probabilities and labels

--- deep_learning_practise/neural_network_classification.py
+++ deep_learning_practise/neural_network_classification.py
@@ -99,3 +99,73 @@ def accuracy_fn(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
     acc = (correct / len(y_pred)) * 100
     return acc
+
+# 7. train model
+
+# view the first 5 predictions of the forwards pass on the test data
+# output of a model is a in the form of logits
+y_logits = model_0(X_test.to(device))[:5]
+
+# use sigmoid to convert the logits to probabilities
+y_pred_probs = torch.sigmoid(y_logits)
+
+# find the predicted labels (rounding the prediction probabilities)
+y_preds = torch.round(y_pred_probs)
+y_pred_labels = torch.round(torch.sigmoid(model_0(X_test.to(device))[:5]))
+
+# removing extra dimension
+y_pred_labels = y_pred_labels.squeeze()
+
+print(f"First 5 predictions:\n{y_pred_labels[:5]}")
+
+torch.manual_seed(42)
+
+# set the number of epochs
+epochs = 100
+
+# putting the data on the target device
+X_train, y_train = X_train.to(device), y_train.to(device)
+x_test, y_test = X_test.to(device), y_test.to(device)
+
+# building training and eval loop
+for epohs in range(epochs):
+    
+    # training
+    model_0.train()
+
+    # 1. Forward pass (model outputs raw logits)
+    y_logits = model_0(X_train).squeeze()
+    # logits -> probabilities -> prediction labels
+    y_pred = torch.round(torch.sigmoid(y_logits))
+
+    # 2. Calculate the loss and accuracy
+    loss = loss_fn(y_logits, y_train)
+    acc = accuracy_fn(y_true=y_train, y_pred=y_pred)
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. loss backwards
+    loss.backward()
+
+    # 5. optimizer step
+    optimizer.step()
+
+    # testing
+    model_0.eval()
+    with torch.inference_mode():
+        # 1.forward pass
+        test_logits = model_0(x_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))
+        # 2. calculate the loss and acccuracy
+        test_loss = loss_fn(test_logits, y_test)
+        test_acc = accuracy_fn(y_true=y_test, y_pred=test_pred)
+
+    # printing the results for 1-10 epochs
+    if epochs % 10 == 0:
+        print(f"Epoch: {epohs} | "
+              f"Train loss: {loss:.5f} | "
+              f"Train acc: {acc:.2f}% | "
+              f"Test loss: {test_loss:.5f} | "
+              f"Test acc: {test_acc:.2f}%")
+        print(f"First 10 test predictions:\n{test_pred[:10]}")
