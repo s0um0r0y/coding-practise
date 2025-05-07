c521f69fdc4e050ecaae1890f8f5f2cb0bcc9337
s0um0r0y <soumoroy09@gmail.com>
2025-05-08 01:49:45 +0530

    hash table practise updated

--- dsa_algo_practise/hash_table_practise.py
+++ dsa_algo_practise/hash_table_practise.py
@@ -14,3 +14,19 @@ class HashTable:
                 return
         self.table[index].append((key, value))
 
+    def get(self, key):
+        index = self.hash_function(key)
+        for k, v in self.table[index]:
+            if k == key:
+                return v
+            return None
+        return None
+    
+    def remove(self, key):
+        index = self.hash_function(key)
+        for i, (k, v) in enumerate(self.table[index]):
+            if k == key:
+                del self.table[index][i]
+                return True
+        return False
+    
