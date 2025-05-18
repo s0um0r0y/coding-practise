2039912c4d3d466ebd6727cf23a101af89ee4499
Soumo Roy <soumoroy09@gmail.com>
2025-05-18 23:38:22 +0530

    DSA practise: AVL tree

--- dsa_algo_practise/tree_AVL_practise.py
+++ dsa_algo_practise/tree_AVL_practise.py
@@ -10,3 +10,36 @@ def getHeight(node):
         return 0
     return node.height
 
+def getBalance(node):
+    if not node:
+        return 0
+    return getHeight(node.left) - getHeight(node.right)
+
+def rightRotate(y):
+    print('rotate right on node', y.data)
+    x = y.left
+    T2 = x.right
+    x.right = y
+    y.left = T2
+    y.height = 1 + max(getHeight(y.left), getHeight(y.right))
+    x.height = 1 + max(getHeight(x.left), getHeight(x.right))
+    return x
+
+def leftRotate(x):
+    print('Rotate left on node',x.data)
+    y = x.right
+    T2 = y.left
+    y.left = x
+    x.right = T2
+    x.height = 1 + max(getHeight(x.left), getHeight(x.right))
+    y.height = 1 + max(getHeight(y.left), getHeight(y.right))
+    return y
+
+def insert(node, data):
+    if not node:
+        return TreeNode(data)
+    
+    if data < node.data:
+        node.left = insert(node.left, data)
+    elif data > node.data:
+        node.right = insert(node.right, data)
