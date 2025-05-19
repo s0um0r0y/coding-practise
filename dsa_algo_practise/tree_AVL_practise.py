bc2b540b62b8a83dd8816f31205913d5af8760d2
Soumo Roy <soumoroy09@gmail.com>
2025-05-19 11:41:35 +0530

    AVL Delete Node

--- dsa_algo_practise/tree_AVL_practise.py
+++ dsa_algo_practise/tree_AVL_practise.py
@@ -82,6 +82,57 @@ def minValueNode(node):
         current = current.left
     return current
 
+def delete(node, data):
+    if not node:
+        return node
+    
+    if data < node.data:
+        node.left = delete(node.left, data)
+    elif data > node.data:
+        node.right = delete(node.right, data)
+    else:
+        if node.left is None:
+            temp = node.right
+            node = None
+            return temp
+        
+        elif node.right is None:
+            temp = node.left
+            node = None
+            return temp
+        
+        temp = minValueNode(node.right)
+        node.data = temp.data
+        node.right = delete(node.right, temp.data)
+
+    if node is None:
+        return node
+    
+    # update the balence factor and balence the tree
+    node.height = 1 + max(getHeight(node.left), getHeight(node.right))
+    balance = getBalance(node)
+
+    # balancing the tree
+    # left left
+    if balance > 1 and getBalance(node.left) >= 0:
+        return rightRotate(node)
+    
+    # left right
+    if balance > 1 and getBalance(node.left) < 0:
+        node.left = leftRotate(node.left)
+        return rightRotate(node)
+    
+    # right right
+    if balance < -1 and getBalance(node.right) <= 0:
+        return leftRotate(node)
+    
+    # right left
+    if balance < -1 and getBalance(node.right) > 0:
+        node.right = rightRotate(node.right)
+        return leftRotate(node)
+    
+    return node
+
 # Inserting nodes
 root = None
 letters = ['C', 'B', 'E', 'A', 'D', 'H', 'G', 'F']
