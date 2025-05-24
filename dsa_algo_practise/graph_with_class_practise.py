52f7cbb510b3da556d3781d41f1616e00a55c264
Soumo Roy <soumoroy09@gmail.com>
2025-05-25 01:09:16 +0530

    added edge code

--- dsa_algo_practise/graph_with_class_practise.py
+++ dsa_algo_practise/graph_with_class_practise.py
@@ -3,3 +3,10 @@ class Graph:
         self.adj_matrix = [[0] * size for _ in range(size)]
         self.size = size
         self.vertex_data = [""] * size
+
+    def add_edge(self, u, v):
+        if 0 <= u < self.size and 0 <= v < self.size:
+            self.adj_matrix[v][v] = 1
+            self.adj_matrix[v][u] =1
+
+    
