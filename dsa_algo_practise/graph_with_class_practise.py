4306520e20ee4e74acfd6bba03462add7f87f4c5
Soumo Roy <soumoroy09@gmail.com>
2025-05-27 22:48:32 +0530

    vertex data is being printed

--- dsa_algo_practise/graph_with_class_practise.py
+++ dsa_algo_practise/graph_with_class_practise.py
@@ -4,10 +4,10 @@ class Graph:
         self.size = size
         self.vertex_data = [""] * size
 
-    def add_edge(self, u, v):
+    def add_edge(self, u, v, weight):
         if 0 <= u < self.size and 0 <= v < self.size:
-            self.adj_matrix[v][v] = 1
-            self.adj_matrix[v][u] =1
+            self.adj_matrix[v][v] = weight
+            # self.adj_matrix[v][u] =1
 
     def add_vertex_data(self, vertex, data):
         if 0 <= vertex < self.size:
@@ -19,7 +19,7 @@ class Graph:
             print(' '.join(map(str, row)))
         print("\nVertex Data:")
         for vertex, data in enumerate(self.vertex_data):
-            print("\nVertex Data:")
+            print(f"Vertex {vertex}: {data}")
 
 g = Graph(4)
 g.add_vertex_data(0, 'A')
@@ -27,9 +27,14 @@ g.add_vertex_data(1, 'B')
 g.add_vertex_data(2, 'C')
 g.add_vertex_data(3, 'D')
 
-g.add_edge(0,1) # A - B
-g.add_edge(0,2) # A - C
-g.add_edge(0,3) # A - D
-g.add_edge(1,2) # B - C
+# g.add_edge(0,1) # A - B
+# g.add_edge(0,2) # A - C
+# g.add_edge(0,3) # A - D
+# g.add_edge(1,2) # B - C
+
+g.add_edge(0, 1, 3)  # A -> B with weight 3
+g.add_edge(0, 2, 2)  # A -> C with weight 2
+g.add_edge(3, 0, 4)  # D -> A with weight 4
+g.add_edge(2, 1, 1)  # C -> B with weight 1
 
 g.print_graph()
