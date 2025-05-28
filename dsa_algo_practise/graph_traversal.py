89476d342565ccb81ab7378d604c99f9231f184c
Soumo Roy <soumoroy09@gmail.com>
2025-05-29 00:33:12 +0530

    graph traversal base code

--- dsa_algo_practise/graph_traversal.py
+++ dsa_algo_practise/graph_traversal.py
@@ -8,3 +8,15 @@ class Graph:
         if 0<=u < self.size and 0 <= v < self.size:
             self.adj_matrix[u][v] = 1
             self.adj_matrix[v][u] = 1
+
+    def add_vertex_data(self, vertex, data):
+        if 0 <= vertex < self.size:
+            self.vertex_data[vertex] = data
+
+    def print_graph(self):
+        print("Adjacency Matrix:")
+        for row in self.adj_matrix:
+            print(" ".join(map(str, row)))
+        print("\nVertex Data:")
+        for vertex, data in enumerate(self.vertex_data):
+            print(f"Vertex {vertex}: {data}")
