e917ac25c48abd8868c8413bfb57954d7a4f46e0
Soumo Roy <soumoroy09@gmail.com>
2025-05-29 13:11:39 +0530

    action server updated

--- ROS_practise/fancy_action_server.py
+++ ROS_practise/fancy_action_server.py
@@ -4,3 +4,21 @@ import rospy
 import time
 import actionlib
 from basics.msg import TimerAction, TimerGoal, TimerResult, TimerFeedback
+
+def feedback_callback(feedback):
+    print('[Feedback] Time elapsed: %f'%(feedback.time_elapsed.to_sec()))
+    print('[Feedback] Time remaining: %f'%(feedback.time_remaining.to_sec()))
+
+rospy.init_node('timer_action_server')
+client = actionlib.SimpleActionClient('timer', TimerAction)
+client.wait_for_server()
+
+goal = TimerGoal()
+goal.time_to_wait = rospy.Duration.from_sec(5.0)
+client.send_goal(goal, feedback_cb=feedback_callback)
+
+client.wait_for_result()
+print('[Result] State: %d'%(client.get_state()))
+print('[Result] Status: %s'%(client.get_goal_status_text()))
+print('[Result] Time elapsed: %f'%(client.get_result().time_elapsed.to_sec()))
+print('[Result] Updates sent: %d'%(client.get_result().updates_sent))
