286774e7d3e4dbe040e4ecef629647d67446ef18
Soumo Roy <soumoroy09@gmail.com>
2025-06-03 00:04:56 +0530

    ROS commit

--- ROS_practise/wanderbot_ws/src/wanderbot/src/red_light_green_light.py
+++ ROS_practise/wanderbot_ws/src/wanderbot/src/red_light_green_light.py
@@ -7,3 +7,12 @@ rospy.init_node('red_light_green_light')
 
 red_light_twist = Twist()
 green_light_twist = Twist()
+green_light_twist.linear.x = 0.5
+
+driving_forward = False
+light_change_time = rospy.Time.now()
+rate = rospy.Rate(10)
+
+# while not rospy.is_shutdown():
+#     if driving_forward:
+        
