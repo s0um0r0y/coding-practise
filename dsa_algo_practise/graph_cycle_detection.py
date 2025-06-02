45f5cbd5aac7d22efb8509a1abcb101ef7c23dd2
Soumo Roy <soumoroy09@gmail.com>
2025-06-03 01:01:47 +0530

    added union find array

---
+++ dsa_algo_practise/graph_cycle_detection.py
@@ -0,0 +1,11 @@
+class Graph:
+    def __init__(self, size):
+        self.adj_matrix = [[0] * size for _ in range(size)]
+        self.size = size
+        self.vertex_data = [''] * size
+        self.parent = [i for i in range(size)] # union find array
+
+    def add_edge(self, u, v):
+        if 0 <= u < self.size and 0 <= v < self.size:
+            self.adj_matrix[u][v] = 1
+            self.adj_matrix[v][u] = 1
