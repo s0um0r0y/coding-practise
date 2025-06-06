4ae16f05397184b8b5fe46c432ae3f7b4a9b0292
s0um0r0y <soumoroy09@gmail.com>
2025-06-06 15:08:12 +0530

    initial graph class commit

--- dsa_algo_practise/dikstra_algo.py
+++ dsa_algo_practise/dikstra_algo.py
@@ -1,3 +1,18 @@
+class Graph:
+    def __init__(self, size):
+        self.adj_matrix = [[0] * size for _ in range(size)]
+        self.size = size
+        self.vertex_data = [''] * size
+
+    def add_edge(self, u ,v, weight):
+        if 0 <= u < self.size and 0 <= v < self.size:
+            self.adj_matrix[u][v] = weight
+            self.adj_matrix[v][u] = weight
+
+    def add_vertex_data(self, vertex, data):
+        if 0 <= vertex < self.size:
+            self.vertex_data[vertex] = data
+
 def dijkstra(self, start_vertex_data):
     start_vertex = self.vertex_data.index(start_vertex_data)
     distances = [float('inf')] * self.size
