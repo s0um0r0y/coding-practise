3a436429acbccb3d96f4d96c9fda14cf51947bed
s0um0r0y <soumoroy09@gmail.com>
2025-06-07 13:11:15 +0530

    bellman ford graph commit

---
+++ dsa_algo_practise/bellman_ford_algo.py
@@ -0,0 +1,11 @@
+class Graph:
+    def __init__(self, size):
+        self.adj_matrix = [[0] * size for _ in range(size)]
+        self.size = size
+        self.vertex_data = [''] * size
+
+    def add_edge(self, u, v, weight):
+        if 0 <= u < self.size and 0 <= v < self.size:
+            self.adj_matrix[u][v] = weight
+
+     
