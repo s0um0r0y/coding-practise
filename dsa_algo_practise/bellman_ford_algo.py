0fb6e39af92dc6ae44ddac6c870d81f37a6161f5
s0um0r0y <soumoroy09@gmail.com>
2025-06-08 18:24:46 +0530

    added edges

--- dsa_algo_practise/bellman_ford_algo.py
+++ dsa_algo_practise/bellman_ford_algo.py
@@ -34,3 +34,12 @@ g.add_vertex_data(2, 'C')
 g.add_vertex_data(3, 'D')
 g.add_vertex_data(4, 'E')
 
+g.add_edge(3, 0, 4)  # D -> A, weight 4
+g.add_edge(3, 2, 7)  # D -> C, weight 7
+g.add_edge(3, 4, 3)  # D -> E, weight 3
+g.add_edge(0, 2, 4)  # A -> C, weight 4
+g.add_edge(2, 0, -3) # C -> A, weight -3
+g.add_edge(0, 4, 5)  # A -> E, weight 5
+g.add_edge(4, 2, 3)  # E -> C, weight 3
+g.add_edge(1, 2, -4) # B -> C, weight -4
+g.add_edge(4, 1, 2)  # E -> B, weight 2
