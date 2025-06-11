d874f9ca7b11840abdefa438c7be736d65b2e8e9
s0um0r0y <soumoroy09@gmail.com>
2025-06-11 16:50:09 +0530

    initial prim algo commit

---
+++ dsa_algo_practise/prim_algo.py
@@ -0,0 +1,16 @@
+class Graph:
+    def __init__(self, size):
+        self.adj_matrix = [[0] * size for _ in range(size)]
+        self.size = size
+        self.vertex_data = [''] * size
+
+    def add_edges(self, u, v, weight):
+        if 0 <= u < self.size and 0 <= v < self.size:
+            self.adj_matrix[u][v] = weight
+            self.adj_matrix[v][u] = weight
+
+    def add_vertex_data(self, vertex, data):
+        if 0<= vertex < self.size:
+            self.vertex_data[vertex] = data
+
+    
