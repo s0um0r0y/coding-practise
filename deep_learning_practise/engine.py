2af7d990f7073ef89b8b587c1c1b61469da100c7
s0um0r0y <soumoroy09@gmail.com>
2025-06-18 14:24:02 +0530

    train commit

--- deep_learning_practise/engine.py
+++ deep_learning_practise/engine.py
@@ -62,3 +62,42 @@ def train_step(model: torch.nn.Module,
     train_loss = train_loss /len(dataloader)
     train_acc = train_acc /len(dataloader)
     return train_loss, train_acc
+
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          loss_fn: torch.nn.Module,
+          optimizer: torch.optim.Optimizer,
+          epochs: int,
+          device: torch.device) -> Dict[str, List]:
+    '''
+        Trains and tests a PyTorch model.
+
+    Passes a target PyTorch models through train_step() and test_step()
+    functions for a number of epochs, training and testing the model
+    in the same epoch loop.
+
+    Calculates, prints and stores evaluation metrics throughout.
+
+    Args:
+        model: A PyTorch model to be trained and tested.
+        train_dataloader: A DataLoader instance for the model to be trained on.
+        test_dataloader: A DataLoader instance for the model to be tested on.
+        optimizer: A PyTorch optimizer to help minimize the loss function.
+        loss_fn: A PyTorch loss function to calculate loss on both datasets.
+        epochs: An integer indicating how many epochs to train for.
+        device: A target device to compute on (e.g. "cuda" or "cpu").
+
+    Returns:
+        A dictionary of training and testing loss as well as training and
+        testing accuracy metrics. Each metric has a value in a list for 
+        each epoch.
+        In the form: {train_loss: [...],
+                    train_acc: [...],
+                    test_loss: [...],
+                    test_acc: [...]} 
+        For example if training for epochs=2: 
+                    {train_loss: [2.0616, 1.0537],
+                    train_acc: [0.3945, 0.3945],
+                    test_loss: [1.2641, 1.5706],
+                    test_acc: [0.3400, 0.2973]} '''
