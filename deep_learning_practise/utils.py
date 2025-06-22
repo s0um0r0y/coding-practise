2c4c369ed7489ade941550f7e110688fcd78a02d
s0um0r0y <soumoroy09@gmail.com>
2025-06-22 16:44:46 +0530

    model saving code completed

--- deep_learning_practise/utils.py
+++ deep_learning_practise/utils.py
@@ -18,3 +18,15 @@ def save_model(model: torch.nn.Module,
                 target_dir="models",
                 model_name="05_going_modular_tingvgg_model.pth")
     """
+    # creating a target directory
+    target_dir_path = Path(target_dir)
+    target_dir_path.mkdir(parents=True,
+                          exist_ok=True)
+    
+    # create model save path
+    assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+    model_save_path = target_dir_path / model_name
+
+    print(f"[INFO] Saving model to: {model_save_path}")
+    torch.save(obj=model.state_dict(),
+             f=model_save_path)
