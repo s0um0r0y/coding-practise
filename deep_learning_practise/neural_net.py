8f8ae07d4deb0903a1ae8f569cb690f8eabc77f6
s0um0r0y <soumoroy09@gmail.com>
2025-06-26 11:44:05 +0530

    feedforward function

--- deep_learning_practise/neural_net.py
+++ deep_learning_practise/neural_net.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import int
 
 class NeuralNetwork:
     def __init__(self, input_size, hidden_size, output_size):
@@ -12,10 +13,17 @@ class NeuralNetwork:
         self.bias_hidden = np.zeros((1, self.hidden_size))
         self.bias_output = np.zeros((1, self.output_size))
 
-    def sigmoid(self, x):
+    def sigmoid(self, x) -> np.ndarray:
         return 1 / (1 + np.exp(-x))
 
-    def sigmoid_derivative(self, x):
+    def sigmoid_derivative(self, x) -> np.ndarray:
         return x * (1 - x) 
 
-       
+    def feedforward(self, X) -> np.ndarray:
+        self.hidden_activation = np.dot(X, self.weights_input_hidden) + self.bias_hidden
+        self.hidden_output = self.sigmoid(self.hidden_activation)
+        self.output_activation = np.dot(self.hidden_output, self.weights_hidden_output) + self.bias_output
+        self.predicted_output = self.sigmoid(self.output_activation)
+        return self.predicted_output
+    
+    def backward()
