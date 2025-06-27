2029d21ebe54fccb7ca05654d97a9d42268b0848
s0um0r0y <soumoroy09@gmail.com>
2025-06-27 21:05:11 +0530

    backpropagation function written

--- deep_learning_practise/neural_net.py
+++ deep_learning_practise/neural_net.py
@@ -26,4 +26,12 @@ class NeuralNetwork:
         self.predicted_output = self.sigmoid(self.output_activation)
         return self.predicted_output
     
-    def backward()
+    def backward(self, X, y, learning_rate) -> None:
+        output_error = y - self.predicted_output
+        output_delta = output_error * self.sigmoid_derivative(self.predicted_output)
+        hidden_error = np.dot(output_delta, self.weights_hidden_output.T)
+        hidden_delta = hidden_error * self.sigmoid_derivative(self.hidden_output)
+        self.weights_hidden_output += np.dot(self.hidden_output.T, output_delta) * learning_rate
+        self.bias_output += np.sum(output_delta, axis=0, keepdims=True) * learning_rate
+        self.weights_input_hidden += np.dot(X.T, hidden_delta) * learning_rate
+        self.bias_hidden += np.sum(hidden_delta, axis=0, keepdims=True) * learning_rate
