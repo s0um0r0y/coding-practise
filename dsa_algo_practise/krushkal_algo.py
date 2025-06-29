fdc21a866fb977c6c811ae2dcbd4e3f7e2bcd011
s0um0r0y <soumoroy09@gmail.com>
2025-06-29 13:39:41 +0530

    krushkal algorithm commit

--- dsa_algo_practise/krushkal_algo.py
+++ dsa_algo_practise/krushkal_algo.py
@@ -12,3 +12,28 @@ class Graph:
         if 0 <= vertex < self.size:
             self.vertex_data[vertex] = data
 
+    def kruskals_algorithm(self):
+        result = [] # minimum spanning tree
+        i=0 # edge counter
+
+        self.edges = sorted(self.edges, key=lambda item: item[2])
+
+        parent, rank = [], []
+
+        for node in range(self.size):
+            parent.append(node)
+            rank.append(0)
+
+        while i < len(self.edges):
+            u, v, weight = self.edges[i]
+            i += 1
+
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+            if x != y:
+                result.append((u, v, weight))
+                self.union(parent, rank, x, y)
+
+        print("Edge \t Weight")
+        for u,v,weight in result:
+            print(f"{self.vertex_data[u]}-{self.vertex_data[v]} \t{weight}")
