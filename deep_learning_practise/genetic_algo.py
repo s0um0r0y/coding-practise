8124db4f77c1a84151cf51eed2a2c1d32fdeae3d
s0um0r0y <soumoroy09@gmail.com>
2025-07-03 11:52:44 +0530

    genectic algorithm function written

--- deep_learning_practise/genetic_algo.py
+++ deep_learning_practise/genetic_algo.py
@@ -9,5 +9,20 @@ def generate_population(size, dim):
     return np.random.rand(size, dim)
 
 # genectic algorithm
-def genetic_algorithm(population, fitness_func, h_generation=100, mutation_rate=0.01):
+def genetic_algorithm(population, fitness_func, n_generation=100, mutation_rate=0.01):
+    for _ in range(n_generation):
+        population = sorted(population, key=fitness_func, reverse=True)
+        next_generation = population[:len(population)//2].copy()
+        while len(next_generation) < len(population):
+            parents_indices = np.random.choice(len(next_generation), 2, replace=False)
+            parent1, parent2 = next_generation[parents_indices[0]], next_generation[parents_indices[1]]
+            crossover_point = np.random.randint(1, len(parent1))
+            child = np.concatenate((parent1[:crossover_point], parent2[crossover_point:]))
+            if np.random.rand() < mutation_rate:
+                mutate_point = np.random.randint(len(child))
+                child[mutate_point] = np.random.rand()
+            next_generation.append(child)
+            population = np.array(next_generation)
+        return population[0]
+    
     
